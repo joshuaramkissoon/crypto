7b828ff7095962654fbabfecc463bd48e8e629e6
Joshua Ramkissoon <joshuaramkissoon@hotmail.com>
2021-05-19 14:33:24 +0100

    added environment and order execution

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import os
 from pprint import pprint
 from binance.client import Client
+from binance.enums import *
 from pricer import PriceStream
-
-api_key = os.environ.get('binance_api')
-api_secret = os.environ.get('binance_secret')
-
-client = Client(api_key, api_secret)
-account = client.get_account()
-balances = account['balances']
-holdings = list(filter(lambda d: float(d['free']) != 0, balances))
-pprint(holdings)
+from environment import Environment
+from order import OrderExecutor
 
 
-stream = PriceStream('btc')
-stream.run()
+config_path = 'config-local.yaml'
+is_live_account = False
+env = Environment(config_path, is_live=is_live_account)
+
+api_key = env.get_variable('api')
+secert_key = env.get_variable('secret')
+
+client = Client(api_key, secert_key, testnet = not is_live_account)
+
+
+# account = client.get_account()
+# pprint(account)
+# balances = account['balances']
+# holdings = list(filter(lambda d: float(d['free']) != 0, balances))
+# pprint(holdings)
+# orders = client.get_all_orders(symbol='ETHGBP')
+# pprint(orders)
+
+exec = OrderExecutor(client)
+symbol = 'ETHUSDT'
+prec, min_notional = exec.prepare_order(symbol)
+# exec.create_order(
+#     SIDE_BUY,
+#     'min',
+#     'ETHGBP',
+#     lot_step=prec
+# )
