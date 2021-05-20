42c1c54ef8ea76acebebd7c8fb5b1cd914100d2f
Joshua Ramkissoon <joshuaramkissoon@hotmail.com>
2021-05-20 22:55:52 +0100

    added algo trader

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ from binance.enums import *
 from crypto import *
 from pprint import pprint
 import concurrent.futures
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 config_path = 'config-local.yaml'
 is_live_account = True       # Set to True for live trading
@@ -14,10 +16,15 @@ secert_key = env.get_variable('secret')
 
 client = Client(api_key, secert_key, testnet = not is_live_account)
 
+# Check user balances
 account = Account(client)
 balances = account.get_portfolio()
 pprint(balances)
 
-stream = PriceStream('ETH')
-# Change PriceStream handle_tick method to implement strategy
-stream.run()
+
+from crypto.algo import AlgoTrader
+# Create trading strategy in strategy.py and import the function below
+from crypto.strategy import trading_strategy
+
+bot = AlgoTrader(client, 'ETH', 'USDT', trading_strategy=trading_strategy)
+bot.trade()
